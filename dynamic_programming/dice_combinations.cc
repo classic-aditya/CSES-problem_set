9b5d9c4212a83820199bd06b8cd2c244dddcee50
unknown <adityaji1906@gmial.com>
2026-08-02 01:00:12 +0530

    better version of dice combination

--- dynamic_programming/dice_combinations.cc
+++ dynamic_programming/dice_combinations.cc
@@ -2,15 +2,15 @@
 #include<vector>
 #define MOD 1000000007
 int main(){
-    int n; std::cin>>n;
-    std::vector<long long> dp(n+1,0);
-    dp[0] = 1;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=6; j++){
-            if(i-j>=0){
-                dp[i] = (dp[i]+dp[i-j])%MOD;
+    int target; std::cin>>target;
+    std::vector<long long> ways(target+1,0);
+    ways[0] = 1;
+    for(int currSum=1; currSum<=target; currSum++){
+        for(int diceVal=1; diceVal<=6; diceVal++){
+            if(currSum-diceVal>=0){
+                ways[currSum] = (ways[currSum]+ways[currSum-diceVal])%MOD;
             }
         }
     }
-    std::cout<<dp[n];
+    std::cout<<ways[target];
 }
